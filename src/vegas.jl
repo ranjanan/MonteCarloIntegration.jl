95b8244f92a623a4ab544f1fa027a5f5f6c2b792
Ranjan Anantharaman <ranjan@dhcp-10-31-111-9.dyn.mit.edu>
2022-09-02 13:20:54 -0400

    fix up adaptive grid

--- src/vegas.jl
+++ src/vegas.jl
@@ -1,5 +1,6 @@
 using Random
 using Distributions
+using QuasiMonteCarlo
 """
     vegas(f, st, en, kwargs...)
 
@@ -67,12 +68,16 @@ function vegas(func,
     ndim = length(lb)
 
     # Start out with uniform grid
-    grid = zeros(nbins, ndim)
-    for d = 1:ndim
-        grid[:,d] .= fill((ub[d] - lb[d])/nbins, nbins)
-    end
-    cgrid = cumsum(grid, dims = 1)
+	x = zeros(nbins+1, ndim)
+	for i = 1:(nbins+1)
+		x[i,:] .= lb + ((ub .- lb) ./ nbins)*(i-1)
+	end
+	delx = zeros(nbins, ndim)
+	for dim = 1:ndim
+		delx[:,dim] .= ((ub[dim] - lb[dim]) / nbins)
+	end
 
+	
     # Initialize all cumulative variables 
     # for integral and sd estimation 
     nevals = 0
@@ -84,36 +89,49 @@ function vegas(func,
 
     while iter <= maxiter
 
+        # Sample `ncalls` points from this grid
+		# Uniform sampling in `y` space
+		ymat = QuasiMonteCarlo.sample(ncalls, lb, ub, QuasiMonteCarlo.UniformSample())	
 
-        # Sample `M` points from this grid
-        pts, bpts = generate_pts(grid, cgrid, ncalls, lb, ub)
-        
+		from_y_to_i(y) = floor(Int,nbins*y) + 1
+		delta(y) = y*nbins + 1 - from_y_to_i(y)
+		from_y_to_x(y,dim) = x[from_y_to_i(y),dim] + delx[from_y_to_i(y),dim]*delta(y)
+		J(y,dim) = nbins*delx[from_y_to_i(y)]
 
-        # Estimate integral. Note: S stands for J*f, S2 is J^2f^2
-		I_mc, avg_S2, fevals = evaluate_at_samples(func, 
-                                            pts, 
-                                            bpts,
-                                            ncalls,
-                                            nbins, 
-                                            grid, 
-                                            batch
-                                           )
+		xmat = similar(ymat)
+		for dim = 1:ndim
+			xmat[dim,:] .= map(y -> from_y_to_x(y, dim), ymat[dim,:])
+		end
+		display(xmat)
+		Js = zeros(eltype(ymat), size(ymat, 2))
+		for i = 1:size(ymat,2)
+			Js[i] = prod(J(ymat[dim, i],dim) for dim = 1:ndim)
+		end
+		imat = map(from_y_to_i, ymat)
 
+        # Estimate integral. 
+		# Get all fevals in one shot
+		if batch
+			fevals = func(collect(eachcol(xmat)))
+			@assert length(fevals) == ncalls 
+		else
+			fevals = map(func, eachcol(xmat))
+		end
+		Jsf = Js .* fevals
 
-        σ² = (avg_S2 - I_mc^2) / (ncalls - 1) + eps() # When σ² = 0
+		integral_mc = sum(Jsf) / ncalls
+		variance_mc = (sum(Jsf.^2) / ncalls - integral_mc^2) / (ncalls - 1)
+
         nevals += ncalls
-        push!(integrals, I_mc)
-        push!(sigma_squares, σ²)
+        push!(integrals, integral_mc)
+        push!(sigma_squares, variance_mc)
 
 		# Now the algorithm tries to make average J^2f^2 the same in every interval
 		# Do this for every dimension
-		d = calculate_d(fevals, bpts, grid)
+		d = calculate_d(Jsf, imat, nbins)
 	
         # Update grid to reflect sub-inc dist
-        grid, cgrid = update_grid(grid, cgrid, d)
-
-        # Update grid and generate new points
-        cumsum!(cgrid, grid, dims=1)
+        x, delx = update_grid(x, delx, d)
 
         oldItot = Itot
         
@@ -140,264 +158,71 @@ function vegas(func,
     χ² = sum(((integrals .- Itot).^2) ./ sigma_squares)
 
 
-    Itot, sd, χ²/(iter-1)
+    #Itot, sd, χ²/(iter-1)
+	x, delx
 end
 
-function evaluate_at_samples(f, pts, bpts, ncalls, nbins, grid, batch)
-
-    S = 0.
-    S² = 0.
-    dim = size(pts, 2)
-
-    # Get all fevals in one shot
-    if batch
-        fevals = f(pts)
-        @assert length(fevals) == ncalls 
-	else
-		fevals = map(f, eachrow(pts))
-	end
-
-    @inbounds for i = 1:ncalls
-
-		fp = fevals[i]
-        # Get probability of that particular point
-        prob = 1.
-        for d = 1:dim
-            prob *= (1/(nbins*grid[bpts[i,d],d]))
-        end
-
-        S += (fp / prob)
-        S² += (fp / prob)^2
-
-    end
-
-    S/ncalls, S²/ncalls, fevals
-end
-
-
-"""
-pts, bpts = generate_pts(grid, cumgrid, M)
-
-Generate `M` points from `grid` which probabilities 
-inversely proportional to grid spacings 
-"""
-function generate_pts(grid, cgrid, ncalls, lb, ub)
-
-    # Get bins and dimension
-    dim = size(cgrid, 2)
-    nbins = size(cgrid, 1)
-
-    # Each dimension needs ncalls points
-    pts = zeros(ncalls, dim)
-    bpts = zeros(Int, ncalls, dim)
-
-    for d = 1:dim
-
-        # Remember which bins they come from 
-        b = rand(1:nbins, ncalls)
-
-        bpts[:,d] .= b
-
-        idx = 1
-        for (i,bin) in enumerate(b)
-            if bin == 1
-                pts[i,d] = lb[d] + rand(Uniform(0, cgrid[1,d]))
-            else
-                st = cgrid[bin-1,d]
-                en = cgrid[bin,d]
-                if st == en
-                    continue
-                end
-                pts[i,d] = lb[d] + rand(Uniform(cgrid[bin-1,d], cgrid[bin,d]))
-            end
-        end
-    end
-
-   pts, bpts
-end
 
 """
 Calculate d matrix.
 """
-function calculate_d(fevals, bpts, grid)
+function calculate_d(Jsf, imat, nbins)
 
-    ncalls = size(bpts, 1) 
-	nbins, ndims = size(grid)
-    d = zeros(nbins, ndims)
-    probs = zeros(size(grid)...)
-	interval_length = map(sum, eachcol(grid))
-
-	# Start with uniform probability everywhere
-	# and then weight by length of grid
-	probmap = fill(1/nbins, nbins, ndims)
-	for i = 1:ndims
-		interval = interval_length[i]
-		for j = 1:nbins
-			probmap[j,i] /= (grid[j,i] / interval)
+	ndim = size(imat,1)
+	d = zeros(nbins, ndim)
+	ncalls = length(Jsf)
+	ni = ncalls / nbins
+	for i = 1:length(Jsf)
+		for dim = 1:ndim
+			d[imat[dim,i],dim] += (Jsf[i]^2 / ni)
 		end
 	end
-    
-	f2 = fevals .^ 2
-    for dim = 1:ndims 
-		for c = 1:ncalls
-			bin_dim = bpts[c,dim]
-			d[bin_dim,dim] += f2[c] ./ (probmap[bin_dim, dim])^2
-		end
-    end
     
 	# Regularize and smooth
 	dreg = copy(d)
-	for i = 1:ndims
-		sumd = sum(d[:,i])
-		dreg[1,i] = (7d[1] + d[2])/8 
+	for dim = 1:ndim
+		sumd = sum(d[:,dim])
+		dreg[1,dim] = (7d[1,dim] + d[2,dim])/8 
 		for j = 2:nbins-1
-			dreg[j,i] = (d[j-1] + 6d[j] + d[j+1]) / 8
+			dreg[j,dim] = (d[j-1,dim] + 6d[j,dim] + d[j+1,dim]) / 8
 		end
-		dreg[end,i] = (d[end-1,i]+ 7d[end]) / 8
-		dreg ./= sumd
+		dreg[end,dim] = (d[end-1,dim]+ 7d[end,dim]) / 8
+		dreg[:,dim] ./= sumd
+	end
+
+	for dim = 1:ndim
+		dreg[:,dim] .= ((1 .- dreg[:,dim]) ./ log.(1 ./ dreg[:,dim])).^1.5
 	end
 	
-	# dreg = mapreduce(x -> ((1 .- x)/log.(1 ./ x)).^0.5,hcat, eachcol(dreg))
 	dreg
 end
 
-function update_grid(grid, cgrid, d)
+function update_grid(x, delx, d)
 
 	nbins, ndims = size(d)
-	newcgrid = copy(cgrid)
+	newx = copy(x)
 	for dim = 1:ndims
-		i = 1
+		newx[1,dim] = x[1,dim]
+		newx[end,dim] = x[end,dim]
+		i=1
+		j=1
+		Sd = 0.
 		delta_d = sum(d[:,dim]) / nbins
 		i+=1
-		while i < nbins
-			Sd = 0.
-			j = 1
-			while Sd < delta_d
-				Sd += d[j, dim]
+		while i < (nbins+1) 
+			while Sd < delta_d 
+				Sd+=d[j, dim]
 				j+=1
 			end
 			Sd -= delta_d
-			newcgrid[i,dim] = cgrid[i,dim] - ((Sd * grid[i,dim])/d[j-1,dim])
+			newx[i,dim] = x[j,dim] - ((Sd * delx[j-1,dim])/d[j-1,dim])
 			i+=1
 		end
 	end
-	newgrid = copy(grid)
-	newgrid[1,:] = newcgrid[1,:]
+	newdelx = copy(delx)
+	newdelx[1,:] .= newx[2,:]
 	for i = 2:nbins
-		newgrid[i,:] .= newcgrid[i,:] .- newcgrid[i-1,:]
+		newdelx[i,:] .= newx[i+1,:] .- newx[i,:]
 	end
-	newgrid, newcgrid
-end
-
-
-#="""
-Calculate
-"""
-function calculate_m_dist(fevals, bpts, grid, dim)
-
- 
-    ncalls = size(bpts, 1) 
-    nbins = size(grid, 1)
-    m = zeros(nbins, dim)
-    probs = zeros(size(grid)...)
-    for d = 1:dim
-        probs[:,d] .= (1 ./ grid[:,d]) ./ sum(1 ./ grid[:,d])
-    end
-    
-    for d = 1:dim 
-        for i = 1:ncalls
-            f̄ = sqrt(fevals[i]^2 / 
-                                (
-                                 (prod(probs[bpts[i,d],:]) / 
-                                 probs[bpts[i,d],d]
-                                ) 
-                                )
-                    )
-            m[bpts[i,d], d] += f̄ * grid[bpts[i,d],d]
-        end
-        m[:,d] .+= sum(m[:,d])
-        m[:,d] .= m[:,d] ./ sum(m[:,d])
-    end
-    
-    m
-end
-
-function update_grid!(grid, cgrid, nbins, ncalls, m)
-
-    dims = size(grid, 2)
-
-    for d = 1:dims
-        gridcol = grid[:,d]
-        mdim = m[:,d]
-
-        # Count zeros in m 
-        z = iszero.(mdim)
-        c = sum(z)
-        pos = findall(z)
-
-        res = zeros(size(grid, 1))  
-        for i = 1:nbins
-            mdim[i] == 0 && continue
-            res[i] = gridcol[i] / mdim[i]
-        end
-
-        # Calculate optm as sum / # non-zero m's
-        optm = sum(mdim)/(nbins - c)
-
-        for i = 1:nbins
-
-            # If there are no m's, grid[i] = 0 
-            if res[i] == 0 
-                gridcol[i] = 0 
-                continue
-            end
-
-            dist  = extract_from_bins!(mdim, optm, grid, res)
-            gridcol[i] = dist
-
-        end
-        grid[:,d] .= gridcol
-    end    
-    
-    grid
-end=#
-
-function extract_from_bins!(m, optm, grid, res)
-
-    nbins = size(m, 1)
-    dist = 0.
-    collected = 0
-    for k = 1:nbins
-
-        # First, calculate how many required
-        required = optm - collected
-
-        (m[k] == 0) && continue
-
-        # If bin has what's required, take everything
-        if required <= m[k]
-            #res = grid[k] / morig[k]
-            dist += (res[k] * required)
-            m[k] -= required
-            collected += required
-        end
-
-        # Update requirements
-        required = optm - collected
-
-        # If more is required, take the entire bin
-        if required >= m[k]
-            #res = grid[k] / morig[k]
-            dist += (res[k] * m[k])
-            collected += m[k]
-            m[k] = 0
-        end
-
-        # If collected everything, exit
-        if collected >= optm
-            break
-        end
-    end
-
-    dist 
+	newx, newdelx
 end
